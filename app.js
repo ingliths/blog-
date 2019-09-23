195927ff34ea649544f3361a9cc37a5899a3fa13
Gabrielle Souza <gabrielle.souza@SAD.PE.GOV.BR>
2019-09-23 10:00:45 -0300

    versão 0.1

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+ var app = express();
+ 
+ app.use(express.static('public'));
+
+ var path = require('path');
+
+ app.all('/', function(req, res){
+  res.sendfile(__dirname + '/public/page/index.html');
+ });
+
+ app.all('/sobre', function(req, res){
+   res.sendfile(__dirname + '/public/page/sobre.html');
+ });
+
+ app.all('/contato', function(req, res){
+   res.sendfile(__dirname + '/public/page/contato.html');
+ });
+
+ app.listen('8081', function(){
+   console.log('Executando!');
+ });
